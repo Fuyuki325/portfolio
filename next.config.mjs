f696a3e3d84258147714cf1bfc11fa59e33208a9
Fuyuki Malahom <fuyuki325@gmail.com>
2024-10-05 17:49:10 -0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // Enables static exports
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,  // Disable the Image Optimization API
+  },
+};
 
 export default nextConfig;

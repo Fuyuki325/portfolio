8d634fa7f1f74a271661f9aa32aa2954ccbf92e7
Fuyuki Malahom <fuyuki325@gmail.com>
2024-10-05 18:02:09 -0700

    _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+// src/pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* You can include custom meta tags, stylesheets, etc. here */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
